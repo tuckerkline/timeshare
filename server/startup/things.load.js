df50759f34401d56199cf421f987f0f16b36d772
Tucker Kline <tucker.kline@gmail.com>
2015-09-22 16:01:00 -0600

    added some info stuff

--- server/startup/things.load.js
+++ server/startup/things.load.js
@@ -1,14 +1,6 @@
 Meteor.startup(function() {
   if(Things.find().count() === 0) {
-    var things = [
-      'Data on the Wire',
-      'One Language',
-      'Database Everywhere',
-      'Latency Compensation',
-      'Full Stack Reactivity',
-      'Embrace the Ecosystem',
-      'Simplicity Equals Productivity'
-    ];
+    var things = []
     things.forEach(function(thing) {
       Things.insert({
         name: thing,
